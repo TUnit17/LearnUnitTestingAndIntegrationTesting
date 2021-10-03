705c006ac46ff58cc9faf1cdcd979366b8f583a2
TUnit17 <thuang177@gmail.com>
2021-10-03 07:28:52 -0700

    itShouldAddTwoNumbers

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//@SpringBootTest
 class DemoApplicationTests {
 
+	Calculator underTest = new Calculator();
+
 	@Test
-	void contextLoads() {
+	void itShouldAddTwoNumbers() {
+		// given
+		int numberOne = 1;
+		int numberTwo = 2;
+
+		// when
+		int actual= underTest.add(numberOne,numberTwo);
+
+		// then
+		int expected = 3;
+		assertEquals(expected,actual); // jUnit
+		assertThat(actual).isEqualTo(expected); //assertJ
+	}
+
+	static class Calculator{
+		int add(int a, int b){
+			return a+b;
+		}
 	}
 
 }
